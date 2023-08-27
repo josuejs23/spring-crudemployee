18512cfae9421333425143f8ff820ced881149f8
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-08-26 22:44:15 -0400

    Removed the rest controller and service by Spring Data Rest

--- src/main/java/com/josuejs23/springboot/crudemployee/dao/EmployeeRepository.java
+++ src/main/java/com/josuejs23/springboot/crudemployee/dao/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.josuejs23.springboot.crudemployee.dao;
 
 import com.josuejs23.springboot.crudemployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// Sorting http://localhost:8080/api/members?sort=lastName,desc
+// Sorting http://localhost:8080/api/members?sort=lastName,firstName,desc
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
